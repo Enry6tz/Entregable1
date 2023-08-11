c6d27fb95f56cf52482bae4a7dda771ea80ac104
Enry Seitz <enrique6tz@gmail.com>
2023-08-11 16:33:38 -0300

    progreso carts

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 let cartIdCounter = 1; // Inicializa el contador en 1
-
 const carts = [];
+
+router.get("/carts", (req, res) => {
+    res.json({ carrito });
+});
+
+
+router.get("/carts/:cid", (req, res) => {
+  const cid = parseInt(req.params.cid);
+  
+  const carritofound = carts.find(cart => cart.id === cid);
+  
+  if (carritofound) {
+    res.json({ status: "success", carritofound });
+  } else {
+    res.status(404).json({ status: "error", message: "Carrito no encontrado" });
+  }
+});
+
 
 router.post('/', (req, res) => {
     const newCart = {
@@ -14,7 +31,39 @@ router.post('/', (req, res) => {
     res.json(newCart);
 });
 
+router.post("/carts/:cid/products/:pid", (req, res) => {
+    try {
+      const cid = parseInt(req.params.cid);
+      const pid = parseInt(req.params.pid);
+  
+      const carrito = carts.find(cart => cart.id === cid);
+      if (!carrito) {
+        return res.status(404).json({ status: "error", message: "Carrito no encontrado" });
+      }
+  
+      // Simplemente agregamos el ID del producto al carrito
+      carrito.products.push(pid);
+      
+      res.json({
+        status: "success",
+        message: "Producto agregado al carrito exitosamente.",
+      });
+    } catch (error) {
+      console.error("Error al agregar el producto al carrito:", error);
+      res
+        .status(500)
+        .json({ status: "error", message: "Error al agregar el producto al carrito." });
+    }
+  });
+  
 
 
 
 module.exports = router;
+
+
+
+
+
+
+
